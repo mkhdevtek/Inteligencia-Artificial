561d932b596c2b884f4f799ac91a4440169ca699
mkhdevtek <ogh142003@gmail.com>
2025-12-10 23:44:20 -0600

    proyecto 4

--- programacion/proyecto4/Capturas P4.md
+++ programacion/proyecto4/Capturas P4.md
@@ -1,1 +1,12 @@
 ![[Pasted image 20251210134443.png]]
+
+![[Pasted image 20251210223530.png]]
+
+![[Pasted image 20251210225734.png]]
+
+![[Pasted image 20251210225803.png]]
+
+![[Pasted image 20251210225824.png]]
+
+![[Pasted image 20251210230658.png]]
+
--- programacion/proyecto4/merge.py
+++ programacion/proyecto4/merge.py
@@ -7,5 +7,5 @@ model = PeftModel.from_pretrained(base_model, "./tutor_algoritmos_lora")
 # Fusionar
 model = model.merge_and_unload()
 model.save_pretrained("./modelo_fusionado")
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
+tokenizer = AutoTokenizer.from_pretrained("unsloth/Llama-3.2-3B-Instruct")
 tokenizer.save_pretrained("./modelo_fusionado")
